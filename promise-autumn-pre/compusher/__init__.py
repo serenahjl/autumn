2518ea1d6809dfa3908e68db86824dea593a4985
hjl <562228104@qq.com>
2018-10-11 14:56:53 +0800

    把api添加到init里

--- promise-autumn-pre/compusher/__init__.py
+++ promise-autumn-pre/compusher/__init__.py
@@ -85,3 +85,17 @@ def make_celery(app):
     return celery
 
 celery = make_celery(app)
+
+
+from .interface import CompnAPI, CompnInstAPI
+from . import api, app
+
+api.add_resource(
+    CompnAPI, '/api/' + app.config['API_VERSION'] + '/compusher/compn',
+    endpoint='compn_ep')
+# api.add_resource(
+#     FilesAPI, '/api/' + app.config['API_VERSION'] + '/compusher/files',
+#     endpoint='compn_files_ep')
+api.add_resource(
+    CompnInstAPI, '/api/' + app.config['API_VERSION'] + '/compusher/compn_inst',
+    endpoint='compn_inst_ep')
